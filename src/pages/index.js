af5dc83982063e42f53952dced9f9dd3e07d460a
elikeith404 <ekbass44@gmail.com>
2022-08-17 19:50:14 -0400

    Added <Head> content

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,10 @@ import Contact from '../components/Contact';
 import Main from '../components/Main';
 import Projects from '../components/Projects/Projects';
 import Skills from '../components/Skills';
+import PreviewImg from '../../public/assets/preview.png';
 
 export async function getStaticProps() {
+	// Connect to Contentful API
 	const client = createClient({
 		space: process.env.CONTENTFUL_ID,
 		accessToken: process.env.CONTENTFUL_ACCESS_KEY,
@@ -15,6 +17,7 @@ export async function getStaticProps() {
 
 	const res = await client.getEntries({ content_type: 'project' });
 
+	// Return projects as a prop, and search for new entries every 5 seconds
 	return {
 		props: {
 			projects: res.items,
@@ -28,9 +31,17 @@ export default function Home({ projects }) {
 		<>
 			<Head>
 				<title>Eli | Fullstack Developer</title>
+				<meta name />
 				<meta
-					name="description"
+					property="og:title"
+					content="Eli-Keith Bass | Fullstack Web Developer"
+					key="title"
+				/>
+				<meta property="og:image" content={PreviewImg} key="image" />
+				<meta
+					property="og:description"
 					content="Portfolio website of fullstack Javascript and React projects by Eli-Keith Bass"
+					key="description"
 				/>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
