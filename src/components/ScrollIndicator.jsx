193a431668076069305f2b7a2270c60feba4a6a9
elikeith404 <ekbass44@gmail.com>
2022-09-03 21:23:31 -0400

    Fixed sizing of scroll indicator to not block Main compenent elements

--- src/components/ScrollIndicator.jsx
+++ src/components/ScrollIndicator.jsx
@@ -19,9 +19,9 @@ const ScrollIndicator = () => {
   return (
     <>
       {visible && (
-        <div className="h-screen fixed flex justify-center items-end w-full pb-10">
+        <div className="bottom-0 fixed flex justify-center w-full pb-10">
           <Link href="/#about">
-            <div className="max-w-[300px] flex justify-center items-center hover-grow animate-pulse">
+            <div className="max-w-[300px] flex justify-center items-center hover-grow animate-pulse -z-10">
               <span className="border-y border-current w-12 mr-2 "></span>
               <HiChevronDoubleDown size={22} />
               <span className="border-y border-current w-12 ml-2 "></span>
